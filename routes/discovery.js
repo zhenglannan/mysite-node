d82829228e1ce13cdbfc3535457dc518c4b0d12c
lanlan <837647279@qq.com>
2020-04-22 23:59:25 +0800

    feature：句子板块路由tags查找

---
+++ routes/discovery.js
@@ -0,0 +1,57 @@
+// 首页句子展示
+var express = require('express');
+var router = express.Router();
+var SentenceModel=require('../modules/TotalSentence')
+// 全局定义currentpage
+var hotCurrentPage=0; 
+var newCurrentPage=0;
+// 每页多少数量
+var perPage=10;
+
+// 最新标签
+router.get('/new',function(req, res, next){
+   // 跳过之前加载过的
+  var skip=(newCurrentPage++)*perPage;
+  SentenceModel.find({}).sort({createdAt:-1}).limit(10).skip(skip).exec(function(err,data){
+    if(err){
+      res.json({
+        status:404,
+        msg:err.message
+      })
+    }else{                                                                      
+      res.send(data)
+    }
+  })
+})
+// 热门句子
+router.get('/hot',function(req,res,next){
+  var skip=(hotCurrentPage++)*perPage;
+  // console.log(skip);
+  SentenceModel.find({}).sort({cntLike:-1}).limit(10).skip(skip).exec(function(err,data){
+    if(err){
+      res.json({
+        status:404,
+        msg:err.message
+      })
+    }else{                                                                      
+      res.send(data)
+    }
+  })
+})
+router.get('/',function(req, res, next){
+  // console.log(req.query.tag);
+  var type=req.query.tag;
+  // 查找数组字段中含有指定值
+  SentenceModel.find({'tags':{$all:[type]}}).exec(function(err,data){
+    if(err){a
+      res.json({
+        status:404,
+        msg:err.message
+      })
+    }else{                                                                      
+      res.send(data)
+    }
+  })
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var homesentence = require('./homesentence');
+var discovery = require('./discovery');
 
 module.exports=function(app){
   app.use('/homesentence',homesentence)
+  app.use('/discovery',discovery)
 }
